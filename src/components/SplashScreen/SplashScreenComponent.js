14f4c62808a413f261d123296f6ab86e096d3048
David Lai <dayvedde@gmail.com>
2016-12-02 01:45:19 -0500

    Added router flux, implemented splash screen interval

--- src/components/SplashScreen/SplashScreenComponent.js
+++ src/components/SplashScreen/SplashScreenComponent.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {Actions} from 'react-native-router-flux';
 
 class SplashScreenComponent extends Component {
+    componentDidMount() {
+        setTimeout(() => {
+            Actions.dashboard();
+        }, 2000)
+    }
+
     render() {
         return (
-            <View>
-                <Text>
+            <View style={{justifyContent: 'center', flex: 1, flexDirection: 'column'}}>
+                <Text style={{fontSize: 24, alignSelf: 'center'}}>
                     AUTO TEXTER SPLASH SCREEN
+                </Text>
+                <Text style={{fontSize: 18, alignSelf: 'center'}}>
+                    This screen will change in 2 seconds
                 </Text>
             </View>
         )
