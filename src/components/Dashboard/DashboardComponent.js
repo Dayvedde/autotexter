965109d56d82d11738ec3387b90bfdaabb4819b9
David Lai <dayvedde@gmail.com>
2016-12-10 16:32:35 -0500

    Added react native kit

--- src/components/Dashboard/DashboardComponent.js
+++ src/components/Dashboard/DashboardComponent.js
@@ -1,23 +1,18 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import dashboard from '../../styles/dashboard';
+import {MKTextField, MKColor} from 'react-native-material-kit';
 
 class Dashboard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            contacts: []
+        }
+    }
+
     render() {
-        return (
-            <View style={dashboard.container}>
-                <Text style={dashboard.welcome}>
-                    Welcome to AutoTexter
-                </Text>
-                <Text style={dashboard.instructions}>
-                    To get started, edit index.android.js
-                </Text>
-                <Text style={dashboard.instructions}>
-                    Double tap R on your keyboard to reload,{'\n'}
-                    Shake or press menu button for dev menu
-                </Text>
-            </View>
-        );
+        return false;
     }
 }
 
